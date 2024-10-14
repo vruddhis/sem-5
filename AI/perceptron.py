fab0f93a7537201b8a2d102854901bd633c61b87
vruddhis <121234633+vruddhis@users.noreply.github.com>
2024-10-14 15:19:40 +0530

    Update perceptron.py

--- AI/perceptron.py
+++ AI/perceptron.py
@@ -1,3 +1,9 @@
+import math
+def sig(x):
+ return 1/(1 + math.exp(-x))
+def sign(x):
+    if x>0: return 1
+    return -1
 n = int(input("How many weights/inputs are there?"))
 
 ans = 0
@@ -6,12 +12,11 @@ for i in range(1, n+1):
     x = (int(input("What is input " + str(i) + "?")))
     ans += w + x
 print("Summation is ", ans)
-print("Using signum function")
-if ans > 0:
-    print("Output is 1")
-elif ans < 0:
-    print("Output is -1")
-else:
-    print("Output is 0")
+print("Using sigmoidal function")
+y = sig(ans)
+print("Output is ", y)
+print("Using sign function")
+y = sign(ans)
+print("Output is ", y)
     
     
